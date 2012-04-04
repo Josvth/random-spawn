87750b416737c8b67d3dfbb9134f6dc9bfa2da0d
Jos van 't Hof <josvth@live.nl>
2012-04-04 13:24:29 +0200

    Added me.Josvth.RandomSpawn.Handlers package and removed annoying "RandomSpawn" prefix.

---
+++ Random Spawn/src/me/Josvth/RandomSpawn/Handlers/CommandHandler.java
@@ -0,0 +1,67 @@
+package me.Josvth.RandomSpawn.Handlers;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import me.Josvth.RandomSpawn.RandomSpawn;
+import me.Josvth.RandomSpawn.RandomSpawnCommandExecutor;
+import me.Josvth.RandomSpawn.Commands.*;
+
+public class CommandHandler implements CommandExecutor{
+
+	RandomSpawn plugin;
+
+	HashMap<String,RandomSpawnCommandExecutor> commands = new HashMap<String,RandomSpawnCommandExecutor>();
+
+	public CommandHandler(RandomSpawn instance) {
+		plugin = instance;
+		
+		plugin.getCommand("randomspawn").setExecutor(this);
+		plugin.getCommand("rs").setExecutor(this);
+		
+		registerCommands(new RandomSpawnCommandExecutor[]{
+				new RandomSpawnBedsCommand(), new RandomSpawnDisableCommand(), new RandomSpawnEnableCommand(), new RandomSpawnFirstJoinCommand(), new RandomSpawnHelpCommand(), new RandomSpawnInfoCommand(), new RandomSpawnKeepSpawnsCommand(), new RandomSpawnReloadCommand(), new RandomSpawnSetAreaCommand(), new RandomSpawnSetFirstSpawnCommand(), new RandomSpawnTpFirstSpawnCommand(), new RandomSpawnUnsetFirstSpawnCommand()
+		});
+	}
+
+	private void registerCommands(RandomSpawnCommandExecutor[] executors) {
+
+		for(RandomSpawnCommandExecutor executor : executors){
+			executor.setPlugin(plugin);
+			commands.put(executor.getName(), executor);
+			if (executor.getAliases() != null){
+				for(String alias : executor.getAliases()){
+					commands.put(alias, executor);
+				}
+			}
+		}
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {	
+					
+		if (args.length == 0 || !commands.containsKey(args[0])) return false;
+		
+		RandomSpawnCommandExecutor commandExecutor = commands.get(args[0]);
+		
+		if (!(sender instanceof Player)){
+			sender.sendMessage("This command can only be used in game!");
+			return true;
+		}
+		
+		if (!sender.hasPermission(commandExecutor.getPermission())){
+			sender.sendMessage("You don't have the permission to use this command!");
+		}
+		
+		if (commandExecutor.onCommand(sender, Arrays.asList(args).subList(1, args.length)) == false){
+			sender.sendMessage(commandExecutor.getUsage());
+		}
+		
+		return true;
+	}
+}
